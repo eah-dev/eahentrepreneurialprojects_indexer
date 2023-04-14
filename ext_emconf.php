58f49c1079175ff3c0f2154d2325d3630a44ed4b
hoelbing <carsten@hoelbing.net>
2023-04-14 15:21:19 +0200

    fix: constraints

--- ext_emconf.php
+++ ext_emconf.php
@@ -6,7 +6,7 @@
  * For the full copyright and license information, please read the
  * LICENSE file that was distributed with this source code.
  *
- * (c) 2022 Carsten Hölbing <carsten.hoelbing@eah-jena.de>, Ernst-Abbe-Hochschule Jena
+ * (c) 2022 Carsten Hoelbing <carsten.hoelbing@eah-jena.de>, Ernst-Abbe-Hochschule Jena
  *
  */
 
@@ -23,12 +23,10 @@
     'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-10.4.99',
+            'typo3' => '10.4.0-11.5.99',
             'ke_search' => '2.5.0-4.99.0',
             'eahentrepreneurialprojects' => '1.1.0-1.2.0',
         ],
-        'conflicts' => [],
-        'suggests' => [],
     ],
     'autoload' => [
         'psr-4' => [
